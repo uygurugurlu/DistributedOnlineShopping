62aff7d21c9fa6e01b148c8e3c22f4eb616aa5de
uyguruygurlu <uygurugurlu@gmail.com>
2021-04-23 09:23:11 -0700

    Added dijkstra algorithm and shortestpaths

--- src/main/scala/distributedonlineshopping.scala
+++ src/main/scala/distributedonlineshopping.scala
@@ -1,5 +1,12 @@
+import org.apache.spark
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.SQLContext
+import org.apache.spark._
+import org.apache.spark.graphx._
+import org.apache.spark.sql.functions.{array, col, explode}
+// To make some of the examples work we will also need RDD
+import org.apache.spark.rdd.RDD
+import org.apache.spark.graphx.lib.ShortestPaths
 
 object distributedonlineshopping {
 
@@ -15,10 +22,117 @@ object distributedonlineshopping {
       .option("header", true)
       .load("./src/main/resources/newdata.csv")
 
-    val rdd = df.rdd
+/*    val rdd = df.rdd
 
-    rdd.take(10).foreach(println)
+    rdd.take(10).foreach(println)*/
+
+    val vertexArray = Array(
+      (1L, ("dist", 28, 20)),
+      (2L, ("cli1", 27, 18)),
+      (3L, ("cli2", 22, 21)),
+      (4L, ("cli3", 21 ,32)),
+      (5L, ("cli4", 21 ,32)),
+    )
+    val edgeArray = Array(
+      Edge(1L, 2L, 3),
+      Edge(1L, 3L, 5),
+      Edge(1L, 4L, 11),
+
+      Edge(2L, 1L, 5),
+      Edge(3L, 1L, 7),
+      Edge(4L, 1L, 11),
+
+      Edge(2L, 3L, 6),
+      Edge(3L, 2L, 6),
+      Edge(5L, 3L, 6),
+
+
+      Edge(2L, 4L, 6),
+      Edge(4L, 2L, 5),
+
+      Edge(3L, 4L, 16),
+      Edge(4L, 3L, 19),
+
+    )
+    /*val columns = Seq("Moment", "number")
+    // Let's create the vertex RDD.
+  val vertices : RDD[(VertexId, String)] = df
+  .select(columns.map(c => col(c)): _*)
+  .distinct // we remove duplicates
+  .rdd.map(_.getAs[String](0)) // transform to RDD
+  .zipWithIndex // associate a long index to each vertex
+  .map(_.swap)*/
+
+    val vertexRDD: RDD[(Long, (String , Int, Int))] = sc.parallelize(vertexArray)
+    val edgeRDD: RDD[Edge[Int]] = sc.parallelize(edgeArray)
+
+    val graph: Graph[(String,Int, Int), Int] = Graph(vertexRDD, edgeRDD)
+
+    //graph.edges.foreach(println)
+
+    println(getShortestPath(getSourceVertex(1L, graph), getSourceVertex(2L, graph), graph))
+
+
+    /*
+        val sourceVertex=graph.vertices.filter { case (id,(_,_,_)) => id == 1L}.first
+        val sourceVertex2=graph.vertices.filter { case (id,(_,_,_)) => id == 2L}.first
+        val dist=sourceVertex._1
+        val cli1=sourceVertex2._1
+        println("----------")
+        println(dist)
+        val result = ShortestPaths.run(graph, Seq(dist))
+        result.edges.foreach(println)
+        result.vertices.foreach(println)
+    */
+
+/*    val shortestPath = result               // result is a graph
+      .vertices                             // we get the vertices RDD
+      .filter({case(vId, _) => vId == dist})  // we filter to get only the shortest path from v1
+      .first                                // there's only one value
+      ._2                                   // the result is a tuple (v1, Map)
+      .get(cli1)*/
 
   }
+  def getSourceVertex(vid: Long, graph: Graph[(String,Int, Int), Int]): VertexId ={
+    val sourceVertex=graph.vertices.filter { case (id,(_,_,_)) => id == vid}.first
+    val res=sourceVertex._1
+    println(res)
+    return res
+  }
+  def getShortestPath(vertexId1: VertexId, vertexId2: VertexId, graph: Graph[(String,Int, Int), Int]): Option[Int] = {
+    val result = ShortestPaths.run(graph, Seq(vertexId1, 2L))
+    println(result.vertices.collect.mkString("\n"))
+    val shortestPath = result               // result is a graph
+      .vertices                             // we get the vertices RDD
+      .filter({case(vId, _) => vId == vertexId1})  // we filter to get only the shortest path from v1
+      .first                                // there's only one value
+      ._2                                   // the result is a tuple (v1, Map)
+      .get(vertexId2)                        // we get its shortest path to v2 as an Option object
+    return shortestPath
+  }
 
+  def dijkstra[VD](g:Graph[(String,Int, Int), Int], origin:VertexId) = {
+    var g2 = g.mapVertices(
+      (vid,vd) => (false, if (vid == origin) 0 else Double.MaxValue))
+
+    for (i <- 1L to g.vertices.count-1) {
+      val currentVertexId =
+        g2.vertices.filter(!_._2._1)
+          .fold((0L,(false,Double.MaxValue)))((a,b) =>
+            if (a._2._2 < b._2._2) a else b)
+          ._1
+
+      val newDistances = g2.aggregateMessages[Double](
+        ctx => if (ctx.srcId == currentVertexId)
+          ctx.sendToDst(ctx.srcAttr._2 + ctx.attr),
+        (a,b) => math.min(a,b))
+
+      g2 = g2.outerJoinVertices(newDistances)((vid, vd, newSum) =>
+        (vd._1 || vid == currentVertexId,
+          math.min(vd._2, newSum.getOrElse(Double.MaxValue))))
+    }
+
+    g.outerJoinVertices(g2.vertices)((vid, vd, dist) =>
+      (vd, dist.getOrElse((false,Double.MaxValue))._2))
+  }
 }
